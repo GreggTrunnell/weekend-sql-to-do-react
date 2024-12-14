5020fb625acea41aa095bb30dcaccd48a70faf8b
Gregg Trunnell <gggtrunnell@gmail.com>
2024-12-14 15:23:24 -0600

    trying to clean the tree

---
+++ src/components/List/List.jsx
@@ -0,0 +1,50 @@
+
+import axios from "axios";
+import DeleteTask from '../deleteTask/deleteTask';
+import TableHead from '../TableHead/TableHead';
+
+function List( {toDos=[] , fetchList } ) {
+   
+    function toggleIsComplete( id, status ){
+        const taskToComplete = {
+            id: id,
+            isComplete: !status,
+        }
+        axios({
+          method: "PUT",
+          url: '/api/todos',
+          data: taskToComplete
+        })
+        .then((response)=>{
+          // console.log("PUT in app.jsx", response.data);
+          fetchList();
+        })
+        .catch((error)=>{
+          console.log("error PUT app.jsx", error)
+        });
+      };
+    return (
+        <div className="List">
+            <table>
+      <TableHead/>
+        <tbody>
+          {toDos.map(( toDo, )=>(
+            <tr key={toDo.id}> 
+              <td>
+                { toDo.text }
+              </td>
+              <td>
+                <button className={toDo.isComplete ? "complete-true" : "complete-false"}
+                onClick={() => toggleIsComplete(toDo.id, toDo.isComplete)}>
+                {toDo.isComplete ? "Completed" : "Not Completed"}</button>
+              </td> 
+              <DeleteTask id={ toDo.id } fetchList={ fetchList }/>
+            </tr>
+          ))}
+          </tbody>
+      </table>  
+        </div>
+    );
+}
+
+export default List;
